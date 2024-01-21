965a9569096b6ff5d2eb3a6a2d5030d48fffa359
unknown <sgrischenko8@gmail.com>
2024-01-21 11:27:10 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -8,7 +8,7 @@ export const fetchCategories = async () => {
   try {
     const response = await axios.get('/shop/categories/');
     const arrayData = response.data;
-    // console.log(response);
+    console.log(response);
     return arrayData;
   } catch (error) {
     console.log(error);
